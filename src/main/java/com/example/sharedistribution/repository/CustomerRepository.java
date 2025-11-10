d9439cbefdd32d60397731a3bc33ad3d6aea44e4
LekhrajJharne26 <jharnelekhraj26@gmail.com>
2025-11-10 19:15:49 +0530

    Initial clean commit - Share Distribution System

--- src/main/java/com/example/sharedistribution/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.sharedistribution.repository;
-
-import com.example.sharedistribution.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
